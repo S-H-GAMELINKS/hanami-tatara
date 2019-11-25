4d82d54492b74f7249c7a63c55761602184481b2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-11-25 23:20:31 +0900

    using Tatara::IntArray

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -1,16 +1,28 @@
 require 'tatara'
 
+class Hoge
+  def initialize
+    @value = "HogeHoge"
+  end
+end
+
 module Web
   module Controllers
     module Home
       class Index
         include Web::Action
 
-        expose :int, :tatara
+        expose :int, :tatara, :hoge, :container
 
         def call(params)
           @int = 42
           @tatara = Tatara::Integer.new
+          @tatara.val = 42
+          @hoge = Hoge.new
+
+          @container = Tatara::IntArray.new
+
+          (1..100).map(&@container.method(:<<))
         end
       end
     end
